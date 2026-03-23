a8269b9d8d3bf4645776f9122d0bc4c9d550bacb
Daniel Vilano Soares <danielvsoares06@gmail.com>
2026-03-23 18:57:59 -0300

    conclusão status packet e adição do codigo de analise de telemetry packet

--- analise_status.py
+++ analise_status.py
@@ -2,7 +2,7 @@ import pandas as pd
 import binascii
 import struct
 
-df = pd.read_csv(r"C:\Users\User\Projeto_F1\dados_interlagos_170226\status.csv", sep = ';')
+df = pd.read_csv(r"C:\Projeto_f1\dados_interlagos_170226\status.csv")
 
 tabela_vazia = []
 
@@ -13,7 +13,7 @@ posicao_inicial = 1074
 
 for i in range(len(df)):
     #escopo raw_text
-    texto_raw = df.iloc[i]['raw_text']
+    texto_raw = df.iloc[i]['raw_hex']
     #transformar em binario
     dados_binario = binascii.unhexlify(texto_raw)
 
@@ -33,4 +33,61 @@ for i in range(len(df)):
     fuel_capacity = struct.unpack('<f', meu_carro[9:13])[0]
     fuel_remaining_laps = struct.unpack('<f', meu_carro[13:17])[0]
     max_rpm = struct.unpack('<H', meu_carro[17:19])[0]
+    idle_rpm = struct.unpack('<H', meu_carro[19:21])[0]
+    max_gears = struct.unpack('<B', meu_carro[21:22])[0]
+    drs_allowed = struct.unpack('<B', meu_carro[22:23])[0]
+    # se nao for hablitado o drs_activation_distance aparece como 0
+    drs_activation_distance = struct.unpack('<H', meu_carro[23:25])[0]
+    actual_compound_tyre = struct.unpack('<B', meu_carro[25:26])[0]
+    visual_compound_tyre = struct.unpack('<B', meu_carro[26:27])[0]
+    tyre_age_laps = struct.unpack('<B', meu_carro[27:28])[0]
+    fia_flags = struct.unpack('<b', meu_carro[28:29])[0]
+    engine_power_ice = struct.unpack('<f', meu_carro[29:33])[0]
+    engine_power_mguk = struct.unpack('<f', meu_carro[33:37])[0]
 
+    #ers em MJ, limte de uso por volta de 4MJ
+    ers_store_energy = struct.unpack('<f', meu_carro[37:41])[0]
+    ers_deploy_mode = struct.unpack('<B', meu_carro[41:42])[0]
+    #recuperação na freada
+    ers_harvested_thislap_mguk = struct.unpack('<f', meu_carro[42:46])[0]
+    #recuperação via calor do turbo
+    ers_harvested_thislap_mguh = struct.unpack('<f', meu_carro[46:50])[0]
+    ers_deployed_thislap = struct.unpack('<f', meu_carro[50:54])[0]
+    network_paused = struct.unpack('<B', meu_carro[54:55])[0]
+
+    linha = {
+        'tempo' : df.iloc[i]['time'],
+        'ABS' : ABS,
+        'fuel_mix' : fuel_mix,
+        'front_brake_bias %' : front_brake_bias,
+        'pit_limiter_status' : pit_limiter_status,
+        'fuel_in_tank' : fuel_in_tank,
+        'fuel_capacity' : fuel_capacity,
+        'fuel_remaining_laps' : fuel_remaining_laps,
+        'max_rpm' : max_rpm,
+        'idle_rpm': idle_rpm,
+        'max_gears' : max_gears,
+        'drs_allowed' : drs_allowed,
+        'drs_activation_distance' : drs_activation_distance,
+        'actual_compound_tyre' : actual_compound_tyre,
+        'visual_compound_tyre' : visual_compound_tyre,
+        'tyre_age_laps' : tyre_age_laps,
+        'fia_flags' : fia_flags,
+        'engine_power_ice' : engine_power_ice,
+        'engine_power_mguk' : engine_power_mguk,
+        'ers_store_energy' : ers_store_energy,
+        'ers_battery %' : (ers_store_energy/4000000) * 100,
+        'ers_deploy_mode' : ers_deploy_mode,
+        'ers_harvested_thislap_MGUK' : ers_harvested_thislap_mguk,
+        'ers_harvested_MGUK %' : (ers_harvested_thislap_mguk/4000000) * 100,
+        'ers_harvested_thislap_MGUH' : ers_harvested_thislap_mguh,
+        'ers_harvested_MGUH %' : (ers_harvested_thislap_mguh/4000000) * 100,
+        'ers_deployed_thislap' : ers_deployed_thislap,
+        'ers_deployed %' : (ers_deployed_thislap/4000000) * 100,
+        'network_paused' : network_paused,
+    }
+
+    tabela_vazia.append(linha)
+
+df_final = pd.DataFrame(tabela_vazia)
+df_final.to_csv("status_data_interlagos_170226.csv", index = False)
---
+++ analise_telemetry.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import struct
+import binascii
+
+df = pd.read_csv(r'C:\Projeto_f1\dados_interlagos_170226\telemetry.csv')
+
+#tabela para armazenar valores
+tabela_vazia = []
+
+#definindo offset (onde o meu carro começa)
+#header (29) + 19 carros * 60 bytes (cada carro usa 60 bytes)
+# 29 + (19 * 60) = 1169 bytes
+posicao_inicial = 1169
+
+for i in range(len(df)):
+    #coluna raw_hex -> binario
+    #selecionando linhas i e coluna raw_hex
+    texto_hex = df.iloc[i]['raw_hex']
+    dados_binarios = binascii.unhexlify(texto_hex)
+
+    meu_carro = dados_binarios[posicao_inicial : posicao_inicial + 60]
+
+    #usando como base o material disponibilizado pela EA
+    #para os desenvolvedores e partindo da leitura e contagem da sequencia de bytes
+    # maiusculo é pq nao tem sinal e minusculo é pq tem
+    velocidade = struct.unpack('<H', meu_carro[0:2])[0]
+    #mutiplica por 100 pq to usando porcentagem
+    acelerador = struct.unpack('<f', meu_carro[2:6])[0]* 100
+    volante = struct.unpack('<f', meu_carro[6:10])[0]
+    freio = struct.unpack('<f', meu_carro[10:14])[0] * 100
+    embreagem = struct.unpack('<B', meu_carro[14:15])[0]
+    marcha = struct.unpack('<b', meu_carro[15:16])[0]
+    rpm = struct.unpack('<H', meu_carro[16:18])[0]
+    drs = struct.unpack('<B', meu_carro[18:19])[0]
+
+    #revlightspercent(19) e revlghtsbitvalue (20-22) nao vou utilzar
+    #4 uint16 = 8 bytes
+    temperatura_freios = struct.unpack('<HHHH', meu_carro[22:30])
+
+    #4uint8 = 4bytes
+    temperatura_superficial_pneu = struct.unpack('<BBBB', meu_carro[30:34])
+    temperatura_interna_pneu = struct.unpack('<BBBB', meu_carro[34:38])
+    temperatura_motor = struct.unpack('<H', meu_carro[38:40])[0]
+    pressao_pneu = struct.unpack('<ffff', meu_carro[40:56])
+    tipo_superfice = struct.unpack('<BBBB', meu_carro[56:60])
+
+    linha = {
+        'tempo': df.iloc[i]['time'],
+        'velocidade': velocidade,
+        'acelerador': acelerador,
+        'volante': volante,
+        'freio': freio,
+        'embreagem': embreagem,
+        'marcha': marcha,
+        'rpm': rpm,
+        'drs': drs,
+        'temperatura_motor': temperatura_motor,
+        #temperatura freio
+        'temp_freio_RL': temperatura_freios[0],
+        'temp_freio_RR': temperatura_freios[1],
+        'temp_freio_FL': temperatura_freios[2],
+        'temp_freio_FR': temperatura_freios[3],
+        #temperatura superficial pneu
+        'temp_sup_pneu_RL': temperatura_superficial_pneu[0],
+        'temp_sup_pneu_RR': temperatura_superficial_pneu[1],
+        'temp_sup_pneu_FL': temperatura_superficial_pneu[2],
+        'temp_sup_pneu_FR': temperatura_superficial_pneu[3],
+        #temperatura interna pneu
+        'temp_int_pneu_RL': temperatura_interna_pneu[0],
+        'temp_int_pneu_RR': temperatura_interna_pneu[1],
+        'temp_int_pneu_FL': temperatura_interna_pneu[2],
+        'temp_int_pneu_FR': temperatura_interna_pneu[3],
+        #pressao pneu
+        'pressao_pneu_RL': round(pressao_pneu[0], 1),
+        'pressao_pneu_RR': round(pressao_pneu[1], 1),
+        'pressao_pneu_FL': round(pressao_pneu[2], 1),
+        'pressao_pneu_FR': round(pressao_pneu[3], 1),
+        #tipo superfice
+        'tipo_superfice_Rl': tipo_superfice[0],
+        'tipo_superfice_RR': tipo_superfice[1],
+        'tipo_superfice_FL': tipo_superfice[2],
+        'tipo_superfice_FR': tipo_superfice[3]
+    }
+
+    tabela_vazia.append(linha)
+
+df_final = pd.DataFrame(tabela_vazia)
+df_final.to_csv("telemtria_f125_interlago170226.csv", index=False)
