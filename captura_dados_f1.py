0ab77fc430fd1929986534967e738e149d9c3518
Daniel Vilano Soares <danielvsoares06@gmail.com>
2026-03-25 10:58:14 -0300

    atualização codigo capturar dados (Refatoração Projeto)

--- captura_dados_f1.py
+++ captura_dados_f1.py
@@ -6,16 +6,22 @@ import os
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("0.0.0.0", 20777))
 
-print("Capturando TODOS os pacotes do F1 25...")
+print("Capturando pacotes do F1 25...")
 
 os.makedirs("packets", exist_ok=True)
 
 files = {
     0: open("packets/motion.csv", "w", newline=""),
+    1: open("packets/session.csv", "w", newline=""),
     2: open("packets/lap.csv", "w", newline=""),
+    3: open("packets/events.csv", "w", newline=""),
+    5: open("packets/setup.csv", "w", newline = ""),
     6: open("packets/telemetry.csv", "w", newline=""),
     7: open("packets/status.csv", "w", newline=""),
-    11: open("packets/history.csv", "w", newline="")
+    10: open("packets/car_damage.csv", "w", newline=""),
+    11: open("packets/history.csv", "w", newline=""),
+    12: open("packets/tyre_extended.csv", "w", newline=""),
+    14: open("packets/time_trial.csv", "w", newline="")
 }
 
 writers = {k: csv.writer(v) for k, v in files.items()}
@@ -32,6 +38,7 @@ def packet_id(data):
 
 
 while True:
+    # data, _, esse _ é o endereço do remetente
     data, _ = sock.recvfrom(4096)
 
     pid = packet_id(data)
